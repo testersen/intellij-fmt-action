4a901d1f84752cab58111754185f6f7a7684eb97
Jameson <james.bradlee@telenor.no>
2023-07-08 01:16:29 +0200

    attempt to fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,5 @@ git config --global --add safe.directory /github/workspace
 cd "/github/workspace" || exit 1
 export JAVA_HOME=$HOME/.sdkman/candidates/java/17.0.2-tem
 styleConfig=${1:-".idea/codeStyles/Project.xml"}
+ls -Rla $(pwd)
 /opt/idea/bin/idea.sh format -d -r -m "*" -s $(pwd)/$styleConfig
